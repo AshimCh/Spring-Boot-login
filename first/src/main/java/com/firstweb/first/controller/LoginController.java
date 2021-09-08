88b8120e629afdad614659426c6059c827138e3c
AshimCh <AshimCh@192.168.1.4>
2021-09-08 22:22:56 +0545

    day2

--- first/src/main/java/com/firstweb/first/controller/LoginController.java
+++ first/src/main/java/com/firstweb/first/controller/LoginController.java
@@ -1,15 +1,38 @@
 package com.firstweb.first.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.firstweb.first.service.LoginService;
 
 @Controller
 public class LoginController {
 	
-	@RequestMapping("/login")
-	public String loginMessage() {
+	@Autowired
+	LoginService service;
+	
+	@RequestMapping
+	(value="/login", method = RequestMethod.GET)
+	public String showLoginPage(ModelMap model){
 		return "login";
+	}
+	@RequestMapping
+	(value="/login", method = RequestMethod.POST)
+	public String showWelcomePage(ModelMap model, @RequestParam String name, @RequestParam String password){
+		
+		
+		boolean isValidUser = service.validateUser(name, password);
+
+        if (isValidUser) {
+            model.put("name", name);
+            return "welcome";
+        } else {
+            model.put("errorMessage", "Invalid Credentials!!");
+            return "login";
+        }
 	}
 }
--- first/src/main/webapp/WEB-INF/jsp/login.jsp
+++ first/src/main/webapp/WEB-INF/jsp/login.jsp
@@ -1,6 +1,13 @@
 <html>
 <head>
 <title>My First Web App</title>
-<body>WALAAAAHIII Wallah</body>
+<body>
+	  <font color="red">${errorMessage}</font>	
+      <form method = "post">
+      Name:<input type = "text" name = "name"/>
+      Password:<input type = "password" name = "password"/>
+      <input type = "Submit"/>
+      </form>
+</body>
 </head>
 </html>
